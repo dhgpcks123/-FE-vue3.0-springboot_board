e47731d765e900839eaa0b3195a450b2365c2a8a
hyechan <dhgpcks123@naver.com>
2022-01-21 18:57:01 +0900

    tailwind 학습

--- src/App.vue
+++ src/App.vue
@@ -27,28 +27,75 @@
     <input
       type="checkbox" 
       class="checked: opacity-50 group-hover:text-pink-400">
-      <span
-        class="block text-sm font-medium text-slate-600 group-hover:text-pink-400"
-      >
-      Username
-      </span>
-      <input
-        type="email"
-        value="test"
-        class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-            focus:border-sky-400 focus:ring-4 focus:ring-violet-400
-              disabled:bg-slate-400 disabled:text-slate-500 disabled:border-slate-200
-              disabled:shadow-none invalid:border-pink-500 invalid:text-pink-400
-              group-hover:text-pink-400
-              peer"
-        required
-      >
-      <div class=" invisible peer-invalid:visible">텍스트가 올바르지 않아요</div>
-      <!-- peer는 형제에만 사용할 수 있다. -->
-      <div class=" hover:bg-regal-blue text-regal-blue">커스텀 색상</div>
-      <div class=" hover:bg-bluesky text-bg-bluesky">커스텀 ex 색상</div>
-
+    <span
+      class="block text-sm font-medium text-slate-600 group-hover:text-pink-400"
+    >
+    Username
+    </span>
+    <input
+      type="email"
+      value="test"
+      class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+          focus:border-sky-400 focus:ring-4 focus:ring-violet-400
+            disabled:bg-slate-400 disabled:text-slate-500 disabled:border-slate-200
+            disabled:shadow-none invalid:border-pink-500 invalid:text-pink-400
+            group-hover:text-pink-400
+            peer"
+      required
+    >
+    <div class=" invisible peer-invalid:visible">텍스트가 올바르지 않아요</div>
+    <!-- peer는 형제에만 사용할 수 있다. -->
+    <div
+      class="hover:bg-regal-blue
+      text-regal-blue after:content-['*']
+      after:ml-1.5 after:text-pink-400 block text-sm"
+    >
+    커스텀 색상
+    </div>
+    <input
+      type="email"
+      placeholder="your@naver.com"
+      class="mt-1 px-3 py-2
+      block w-full rounded-md
+      bg-yellow-50 border shadow-sm border-slate-400 
+      placeholder-pink-400 placeholder:italic placeholder 
+      focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1
+      "
+    >
+  <span
+    class="before:block before:absolute before:-inset-1 before:-skew-y-3
+    before:bg-pink-500 relative inline-block">
+    <span class="relative text-white">사랑해</span>
+  </span>
+    <input type="file" class="block w-full text-sm text-slate-500
+      file:mr-4 file:py-2 file:px-4
+      file:rounded-full file:border-0
+      file:text-sm file:font-semibold
+      file:bg-violet-50 file:text-violet-700
+      hover:file:bg-violet-100
+    "/>
+  <ul 
+    role="list"
+    class="marker:text-sky-400 list-disc pl-5 space-y-3 text-slate-500"
+  >
+    <li>5 cups chopped Porcini mushrooms</li>
+    <li>1/2 cup of olive oil</li>
+    <li>3lb of celery</li>
+  </ul>
   </form>
+  <div class="
+    selection:bg-fuchsia-300 selection:text-fuchsia-900
+  ">
+    <p class="
+      first-letter:text-7xl first-letter:mr-3 first-letter:float-left
+    ">
+      So I started to walk into the water. I won't lie to you boys, I was
+      terrified. But I pressed on, and as I made my way past the breakers
+      a strange calm came over me. I don't know if it was divine intervention
+      or the kinship of all living things but I tell you Jerry at that moment,
+      I <em>was</em> a marine biologist.
+    </p>
+  </div>
 
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,27 @@
 module.exports = {
+  // purge: {
+    // enabled: true, tailwind 기본적으로 JIT엔진 사용
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
+  // darkMode: false, // or 'media' or 'class'
   theme: {
-    screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
-    },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-    },
-    colors: {
-      'regal-blue': '#243c5a',
-    },
+    // screens: {
+    //   'sm': '640px',
+    //   'md': '768px',
+    //   'lg': '1024px',
+    //   'xl': '1280px',
+    //   '2xl': '1536px',
+    // },
+    // fontFamily: {
+    //   sans: ['Graphik', 'sans-serif'],
+    //   serif: ['Merriweather', 'serif'],
+    // },
+    // colors: {
+    //   'regal-blue': '#243c5a',
+    // },
+    //fontFamily? fontsize정도 빼고는 확장으로 써야겠네
     extend: {
       // 테마 옵션의 기본 값을 유지하면서 새 값도 추가하려면 extend
       colors: {
