fef4cf4721a3121dcf0e7255a9467caedc06d23d
hyechan <dhgpcks123@naver.com>
2022-01-21 16:51:36 +0900

    vite로 vue FE 프로젝트 설정

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <button class=" bg-blue-300">
+    hover
+  </button>
+  <button class=" bg-indigo-300 hover:bg-blue-600">
+    중간 중단점 수정
+  </button>
+  <button class=" bg-sky-300 hover:bg-blue-700">
+  </button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
